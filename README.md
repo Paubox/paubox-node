c0f17939391f1bf90362049ac2a47cd1f191d966
Jonathan Greeley <jon.r.greeley@gmail.com>
2018-08-30 12:07:46 -0700

    compile es6 and ignore linting lib folder

---
+++ README.md
@@ -0,0 +1,19 @@
+## Setting up Environment Variables
+Create a .env file in the project root directory and add the following constants
+```
+TEST_SENDER="sender@yourdomain.com"
+TEST_RECIPIENT="recipient@receivingdomain.com"
+TEST_API_KEY="insert API key"
+TEST_USERNAME="API endpoint username"
+```
+
+## Compiling ES6 to ES5 in Development
+```bash
+npx babel --presets env ./src --out-dir ./lib
+```
+
+## Running Tests
+```
+node test
+
+```
---
+++ src/paubox.js
@@ -0,0 +1,33 @@
+"use strict";
+require("dotenv").config();
+const message = require("./message");
+
+class Paubox {
+  constructor(options) {
+    if (!options.apiKey) {
+      throw new Error("apiKey is missing.");
+    }
+    if (!options.endpointUsername) {
+      throw new Error("endpointUsername is missing.");
+    }
+    this.apiKey = options.apiKey;
+    this.endpointUsername = options.endpointUsername;
+    this.protocol = options.protocol || "https:";
+    this.host = options.host || "api.paubox.net";
+    this.port = options.protocol || 443;
+
+    this.options = {
+      apiKey: this.apiKey,
+      endpointUsername: this.endpointUsername,
+      baseUrl: `${this.protocol}//${this.host}/${this.endpointUsername}`
+    };
+  }
+  message(options) {
+    console.log(options);
+    return message(options);
+  }
+}
+
+module.exports = function(options) {
+  return new Paubox(options);
+};
