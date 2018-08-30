c0fe9297124c1c2c4a7193f18e91b334a0115131
Jonathan Greeley <jon.r.greeley@gmail.com>
2018-08-30 11:46:58 -0700

    initial commit

---
+++ test/data/config.js
@@ -0,0 +1,26 @@
+const environments = {};
+
+environments.test = {
+    from: process.env.TEST_SENDER,
+    to: process.env.TEST_RECIPIENT,
+    subject: "Test Runner",
+    text: "Text content",
+    apiKey: process.env.TEST_API_KEY,
+    endpointUsername: process.env.TEST_USERNAME
+};
+
+environments.dev = {
+    from: process.env.DEV_SENDER,
+    to: process.env.DEV_RECIPIENT,
+    subject: "Test Runner",
+    text: "Text content",
+    apiKey: process.env.DEV_API_KEY,
+    endpointUsername: process.env.DEV_USERNAME
+};
+
+const currentEnvironment = typeof ( process.env.NODE_ENV ) == "string" ? process.env.NODE_ENV.toLowerCase() : "";
+
+const environmentToExport = typeof ( environments[ currentEnvironment ] ) == "object" ? environments[ currentEnvironment ] : environments.dev;
+
+module.exports = environmentToExport;
+
