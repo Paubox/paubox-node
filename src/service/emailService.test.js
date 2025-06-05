2dbcd1433ea21dde7e6b1dd017699818ffc8e4e7
Bradley Marques <bradleyrcmarques@gmail.com>
2025-06-05 09:55:33 +0200

    Another red test

--- src/service/emailService.test.js
+++ src/service/emailService.test.js
@@ -318,8 +318,8 @@ describe('emailService.SendBulkMessages', function () {
 
   const messageAlice = Message({
     from: 'reception@authorized_domain.com',
-    reply_to: 'reception@authorized_domain.com',
-    to: ['alice@example.com'],
+    reply_to: 'Reception <reception@authorized_domain.com>',
+    to: ['alice@example.com', 'Alice Anderson <alice@host.com>'],
     cc: ['accounts@authorized_domain.com'],
     bcc: null,
     subject: 'Hi Alice!',
@@ -327,15 +327,21 @@ describe('emailService.SendBulkMessages', function () {
     forceSecureNotification: false,
     text_content: 'Hi Alice!',
     html_content: '<html><body><h1>Hi Alice!</h1></body></html>',
-    attachments: null,
+    attachments: [
+      {
+        fileName: "hello_alice.txt",
+        contentType: "text/plain",
+        content: "SGVsbG8gQm9iIQ=="
+      }
+    ],
     list_unsubscribe: null,
     list_unsubscribe_post: null,
   });
 
   const messageBob = Message({
     from: 'reception@authorized_domain.com',
-    reply_to: 'reception@authorized_domain.com',
-    to: ['bob@example.com'],
+    reply_to: 'Reception <reception@authorized_domain.com>',
+    to: ['bob@example.com', 'Bob Brown <bob@host.com>'],
     cc: ['accounts@authorized_domain.com'],
     bcc: null,
     subject: 'Hi Bob!',
@@ -343,13 +349,112 @@ describe('emailService.SendBulkMessages', function () {
     forceSecureNotification: false,
     text_content: 'Hi Bob!',
     html_content: '<html><body><h1>Hi Bob!</h1></body></html>',
-    attachments: null,
+    attachments: [
+      {
+        fileName: "hello_bob.txt",
+        contentType: "text/plain",
+        content: "SGVsbG8gQm9iIQ=="
+      }
+    ],
     list_unsubscribe: null,
     list_unsubscribe_post: null,
   });
 
   this.afterEach(() => {
     axiosStub.restore();
+  });
+
+  it('posts the correct JSON payload to the correct Paubox API endpoint', async function () {
+    const expectedPayload = JSON.stringify({
+      data: {
+        messages: [
+          {
+            recipients: [
+              "alice@example.com",
+              "Alice Anderson <alice@host.com>"
+            ],
+            cc: [
+              "accounts@authorized_domain.com"
+            ],
+            bcc: null,
+            headers: {
+              subject: "Hi Alice!",
+              from: "reception@authorized_domain.com",
+              "reply-to": "Reception <reception@authorized_domain.com>",
+            },
+            allowNonTLS: false,
+            forceSecureNotification: false,
+            content: {
+              "text/plain": "Hi Alice!",
+              "text/html": Buffer.from("<html><body><h1>Hi Alice!</h1></body></html>").toString('base64')
+            },
+            attachments: [
+              {
+                fileName: "hello_alice.txt",
+                contentType: "text/plain",
+                content: "SGVsbG8gQm9iIQ=="
+              }
+            ]
+          },
+          {
+            recipients: [
+              "bob@example.com",
+              "Bob Brown <bob@host.com>"
+            ],
+            cc: [
+              "accounts@authorized_domain.com"
+            ],
+            bcc: null,
+            headers: {
+              subject: "Hi Bob!",
+              from: "reception@authorized_domain.com",
+              "reply-to": "Reception <reception@authorized_domain.com>",
+            },
+            allowNonTLS: false,
+            forceSecureNotification: false,
+            content: {
+              "text/plain": "Hi Bob!",
+              "text/html": Buffer.from("<html><body><h1>Hi Bob!</h1></body></html>").toString('base64')
+            },
+            attachments: [
+              {
+                fileName: "hello_bob.txt",
+                contentType: "text/plain",
+                content: "SGVsbG8gQm9iIQ=="
+              }
+            ]
+          }
+        ]
+      }
+    });
+
+    let capturedConfig;
+    axiosStub = sinon.stub(axios, 'create').returns(function (config) {
+      capturedConfig = config;
+      return Promise.resolve({
+        data: {
+          messages: [
+            {
+              sourceTrackingId: "3d38ab13-0af8-4028-bd45-999999999999",
+              customHeaders: null,
+              data: "Service OK"
+            },
+            {
+              sourceTrackingId: "3d38ab13-0af8-4028-bd45-000000000000",
+              customHeaders: null,
+              data: "Service OK"
+            }
+          ]
+        },
+      });
+    });
+
+    const service = emailService(testCredentials);
+    await service.sendBulkMessages([messageAlice, messageBob]);
+
+    expect(capturedConfig.method).to.equal('POST');
+    expect(capturedConfig.url).to.equal('/bulk_messages');
+    expect(capturedConfig.data).to.deep.equal(expectedPayload);
   });
 
   it('can return a successful response', async function () {
