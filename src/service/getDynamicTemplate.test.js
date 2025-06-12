6c4f4660f82addf6448930c2f8eea0b749c2cc1a
Bradley Marques <bradleyrcmarques@gmail.com>
2025-06-12 11:18:34 +0200

    [WIP] Splits tests into their own files

---
+++ src/service/getDynamicTemplate.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const { expect } = chai;
+const chaiAsPromised = require('chai-as-promised').default;
+
+const sinon = require('sinon');
+const axios = require('axios');
+const fs = require('fs');
+
+const emailService = require('./emailService.js');
+
+chai.use(chaiAsPromised);
+
+describe('emailService.getDynamicTemplate', function () {
+  it('can get a dynamic template', async function () {
+    this.skip();
+  });
+});
---
+++ src/service/listDynamicTemplates.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const { expect } = chai;
+const chaiAsPromised = require('chai-as-promised').default;
+
+const sinon = require('sinon');
+const axios = require('axios');
+const fs = require('fs');
+
+const emailService = require('./emailService.js');
+
+chai.use(chaiAsPromised);
+
+describe('emailService.listDynamicTemplates', function () {
+  it('can list dynamic templates', async function () {
+    this.skip();
+  });
+});
